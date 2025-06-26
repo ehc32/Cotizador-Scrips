4a6f0af163b15e9358f7b219a49fc93caa7a5c6a
caya <caya@ec.krugercorp.com>
2025-06-25 22:07:45 -0500

    Update-Node-js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ async function guardarEnGoogleSheets(data) {
   ];
   await sheets.spreadsheets.values.append({
     spreadsheetId: SHEET_ID,
-    range: "Sheet1!A:F",
+    range: "Sheet1",
     valueInputOption: "USER_ENTERED",
     resource: { values: [fila] },
   });
